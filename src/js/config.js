ca05c8b3ebc1364f22e330934c7c188b808d4ad3
Danny Vu <macintosh@Dannys-MacBook-Air.local>
2019-02-22 17:13:12 +0100

    show ingredient count in fractions and highlight selected

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,6 @@
 // key ed73daf4292565544fb5ee1f0002989e
 // key d5d3ce7ef708dd7657f1329a4de4739e
 // key b4ee6d915129a5d32f76b0a10a4df840
+// key e171682bdcdff058abdf8b71c6c4c7c8
 export const proxy = 'https://cors-anywhere.herokuapp.com'
-export const key = 'b4ee6d915129a5d32f76b0a10a4df840';
+export const key = 'e171682bdcdff058abdf8b71c6c4c7c8';
