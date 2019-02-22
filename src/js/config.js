b9bbee9d57d6d66fb9c9a1f29a806d319627fe5e
Danny Vu <macintosh@Dannys-MacBook-Air.local>
2019-02-22 15:21:38 +0100

    start building recipe view

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,5 @@
 // key ed73daf4292565544fb5ee1f0002989e
 // key d5d3ce7ef708dd7657f1329a4de4739e
+// key b4ee6d915129a5d32f76b0a10a4df840
 export const proxy = 'https://cors-anywhere.herokuapp.com'
-export const key = 'd5d3ce7ef708dd7657f1329a4de4739e';
+export const key = 'b4ee6d915129a5d32f76b0a10a4df840';
